d019cd658ceea3b707a3aed59aea411f3b6a8a22
Kishan Modi <kishandmodi@gmail.com>
2023-12-13 09:26:54 -0500

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HabitTracker from './screens/HabitTracker';
+import Home from './screens/Home';
 
-function App() {
-    return <div>App</div>;
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route
+                    path='/'
+                    element={<Home />}
+                />
+                <Route
+                    path='/tracker'
+                    element={<HabitTracker />}
+                />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
